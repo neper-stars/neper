2904e1fa644e5c640aaf06a342bd5628fe3f73dd
neper-stars <252487497+neper-stars@users.noreply.github.com>
2025-12-11 15:17:14 +0100

    add a test to prove private sessions are created private

--- restapi/handlers/session_create_test.go
+++ restapi/handlers/session_create_test.go
@@ -65,5 +65,47 @@ func TestSessionCreateHandler(t *testing.T) {
 		require.Equal(t, session.Name, shireSession.Name)
 		require.Equal(t, 1, len(shireSession.Managers))
 		require.Equal(t, "merryID", shireSession.Managers[0])
+		require.Equal(t, false, shireSession.Private)
+	})
+
+	t.Run("create mordor", func(t *testing.T) {
+		// here we do not test authorizations which are applied in the
+		// layer just above the handler we are testing.
+		// We can only test if the returned content is coherent with
+		// our current user
+		mordor := &models.Session{
+			Managers: []string{"merryID"}, // merry is one of our users !!
+			Name:     "Mordor",
+			Private:  true,
+		}
+		merryPrincipal := &models.Principal{
+			StandardClaims: jwt.StandardClaims{
+				Subject:   "merryID",
+				ExpiresAt: time.Now().Add(time.Minute).Unix(),
+			},
+			IsGlobalManager: false,
+		}
+		params := operations.SessionCreateParams{
+			Session: mordor,
+		}
+		session, err := createHandler.handle(ctx, params, merryPrincipal)
+
+		require.NoError(t, err)
+		require.Equal(t, "Mordor", session.Name)
+		// ID is a readOnly field and will be filled with something by the API
+		require.NotEqual(t, "", session.ID)
+		require.Equal(t, []string{"merryID"}, session.Managers)
+
+		// reread from our api to see if members are correctly set
+		readParams := operations.SessionReadParams{
+			SessionID: session.ID,
+		}
+		mordorSession, err := readHandler.handle(ctx, readParams, merryPrincipal)
+		require.NoError(t, err)
+		require.Equal(t, session.Name, mordorSession.Name)
+		require.Equal(t, 1, len(mordorSession.Managers))
+		require.Equal(t, "merryID", mordorSession.Managers[0])
+		// mordor was made private on purpose! so it should be returned as private
+		require.Equal(t, true, mordorSession.Private)
 	})
 }
